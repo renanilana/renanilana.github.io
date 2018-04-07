c485d11bf543ac1dbca843937469b65682297ef1
renansaatman <renansaatman@hotmail.com>
2018-04-07 17:15:38 -0300

    comecando o ts

---
+++ codigos/tiltshift.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <cmath> 
+
+using namespace cv;
+using namespace std;
+
+double alfa;
+int alfa_slider = 0;
+int alfa_slider_max = 100;
+
+int top_slider = 0;
+int top_slider_max = 100;
+
+Mat image1, image2, blended;
+Mat imageTop;
+
+char TrackbarName[50];
+
+void on_trackbar_blend(int, void*){
+ alfa = (double) alfa_slider/alfa_slider_max ;
+ addWeighted( image1, alfa, imageTop, 1-alfa, 0.0, blended);
+ imshow("addweighted", blended);
+}
+
+void on_trackbar_line(int, void*){
+  image1.copyTo(imageTop);
+  int limit = top_slider*480/100;
+  if(limit > 0){
+	Mat tmp = image2(Rect(0, 0, 640, limit));
+	tmp.copyTo(imageTop(Rect(0, 0, 640, limit)));
+  }
+  on_trackbar_blend(alfa_slider,0);
+}
+
+int main(int argvc, char** argv){
+  image1 = imread("aero.jpg");
+  resize(image1,image1,Size(640,480));
+  image1.copyTo(image2);
+  namedWindow("addweighted", 1);
+  image2.convertTo(image2,CV_32F);
+  //filtro media--------------------------------------------
+  float media[]={1,1,1,
+               1,1,1,
+               1,1,1};
+  Mat mascara;
+  mascara = Mat(3,3,CV_32F,media);
+  scaleAdd(mascara, 1/9.0, Mat::zeros(3,3,CV_32F), mascara);
+  //--------------------------------------------------------
+
+  for (int i = 0; i < 7; i++) {
+        filter2D(image2, image2, image2.depth(), mascara, Point(1,1), 0);
+  }
+  image2.convertTo(image2, CV_8U);
+  image2.copyTo(imageTop);
+  sprintf( TrackbarName, "Alpha x %d", alfa_slider_max );
+  createTrackbar( TrackbarName, "addweighted",
+				  &alfa_slider,
+				  alfa_slider_max,
+				  on_trackbar_blend );
+  on_trackbar_blend(alfa_slider, 0 );
+  
+  sprintf( TrackbarName, "Scanline x %d", top_slider_max );
+  createTrackbar( TrackbarName, "addweighted",
+				  &top_slider,
+				  top_slider_max,
+				  on_trackbar_line );
+  on_trackbar_line(top_slider, 0 );
+
+  waitKey(0);
+  return 0;
+}
