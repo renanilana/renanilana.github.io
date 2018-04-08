958e3a5e7b60777a6926d328592fd41e01ac3c02
renansaatman <renansaatman@hotmail.com>
2018-04-07 23:11:17 -0300

    tilt

--- codigos/tiltshift.cpp
+++ codigos/tiltshift.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <opencv2/opencv.hpp>
 #include <cmath> 
@@ -6,12 +7,16 @@ using namespace cv;
 using namespace std;
 
 double alfa;
+
+// POSICAO
+int center_slider=0;
+int center_slider_max=100;
+// DECAIMENTO
 int alfa_slider = 0;
 int alfa_slider_max = 100;
-
+// ALTURA
 int top_slider = 0;
 int top_slider_max = 100;
-
 Mat image1, image2, blended;
 Mat imageTop;
 
@@ -19,17 +24,35 @@ char TrackbarName[50];
 
 void on_trackbar_blend(int, void*){
  alfa = (double) alfa_slider/alfa_slider_max ;
- addWeighted( image1, alfa, imageTop, 1-alfa, 0.0, blended);
+ addWeighted( image2, alfa, imageTop, 1-alfa, 0.0, blended);
  imshow("addweighted", blended);
 }
 
 void on_trackbar_line(int, void*){
-  image1.copyTo(imageTop);
-  int limit = top_slider*480/100;
+  image2.copyTo(imageTop);
+  
+  Size tamanho = image2.size();
+
+  int largura = tamanho.width;
+  int altura = tamanho.height;
+  int limit = top_slider*altura/100;
+  int base = center_slider*altura/100;
+
+ 
   if(limit > 0){
-	Mat tmp = image2(Rect(0, 0, 640, limit));
-	tmp.copyTo(imageTop(Rect(0, 0, 640, limit)));
+
+    
+    if(base >= 0 && base <= altura-limit){
+      Mat tmp = image1(Rect(0, base, largura,limit));
+      tmp.copyTo(imageTop(Rect(0, base, largura,limit)));
+    }
+
+    else{
+      Mat tmp = image1(Rect(0, 0, largura,limit));
+      tmp.copyTo(imageTop(Rect(0, 0, largura,limit)));
+    }
   }
+
   on_trackbar_blend(alfa_slider,0);
 }
 
@@ -53,20 +76,28 @@ int main(int argvc, char** argv){
   }
   image2.convertTo(image2, CV_8U);
   image2.copyTo(imageTop);
-  sprintf( TrackbarName, "Alpha x %d", alfa_slider_max );
+  sprintf( TrackbarName, "Decaimento x %d", alfa_slider_max );
   createTrackbar( TrackbarName, "addweighted",
-				  &alfa_slider,
-				  alfa_slider_max,
-				  on_trackbar_blend );
+          &alfa_slider,
+          alfa_slider_max,
+          on_trackbar_blend );
   on_trackbar_blend(alfa_slider, 0 );
   
-  sprintf( TrackbarName, "Scanline x %d", top_slider_max );
+  sprintf( TrackbarName, "Altura x %d", top_slider_max );
   createTrackbar( TrackbarName, "addweighted",
-				  &top_slider,
-				  top_slider_max,
-				  on_trackbar_line );
+          &top_slider,
+          top_slider_max,
+          on_trackbar_line );
   on_trackbar_line(top_slider, 0 );
 
+  sprintf( TrackbarName, "Posição x %d", top_slider_max );
+  createTrackbar( TrackbarName, "addweighted",
+          &center_slider,
+          center_slider_max,
+          on_trackbar_line );
+  on_trackbar_line(center_slider, 0 );
+
   waitKey(0);
+  imwrite("tiltshift.png", blended);
   return 0;
 }
